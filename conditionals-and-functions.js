0130be5513e525aa5f9e82f524fc7abe3a35cfbc
Taylor Smith <taylorleesmith92@gmail.com>
2018-12-14 17:57:14 -0700

    added conditionals and functions

---
+++ conditionals-and-functions.js
@@ -0,0 +1,31 @@
+/*
+* Conditionals, Functions, Scope, and Loops
+*/
+
+// Equals
+
+let equals = 1 === 1;
+
+// Greater Than
+let greaterThan = 5 > 1;
+
+// Less Than
+let lessThan = 2 < 10;
+
+// Greater Than Equal To
+let greaterThanEq = 5>=5;
+
+// Less Than Equal To
+let lessThanEq = 4 <= 9;
+
+// Not Equals
+let notEquals = 5 !== 2;
+
+//              0  1  2  3  4  5  6
+let ourArray = [1, 2, 3, 4, 5, 6, 7];
+
+let arrLen= ourArray.length;
+//for(let i=0; i<arrLen; i++) {
+	//console.log("i is equal to " + i;
+	console.log(ourArray[i]);
+//}
